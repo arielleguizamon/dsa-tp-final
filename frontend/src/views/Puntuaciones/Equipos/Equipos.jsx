c219ac6559df83370498325f9c7be82e3bffcbb5
sruta <santiago.ruta@tiarg.com.ar>
2018-09-17 23:30:06 -0300

    Puntuaciones improvement

--- frontend/src/views/Puntuaciones/Equipos/Equipos.jsx
+++ frontend/src/views/Puntuaciones/Equipos/Equipos.jsx
@@ -1,13 +1,46 @@
 import React from "react";
 import DefaultCard from "../../../components/DefaultCard";
+import Select from "../../../components/Select";
 import Table from "../../../components/STable";
+import { Row, Col, Button } from "reactstrap";
+import {
+  updateForm,
+  validateField,
+  validateForm,
+  populateErrors
+} from "../../../lib/forms/utils";
+import { required } from "../../../lib/forms/validators";
 
 export default class Equipos extends React.Component {
+  updateForm;
+  updateField;
+  validateForm;
+  populateErrors;
+
   constructor(props) {
     super(props);
     this.state = {
-      data: []
+      data: [],
+      equipos: [],
+      formBusqueda: {
+        juego: {
+          value: undefined,
+          errors: undefined,
+          validate: required()
+        },
+        equipo: {
+          value: undefined,
+          errors: undefined,
+          validate: required()
+        }
+      }
     };
+
+    this.updateForm = updateForm.bind(this);
+    this.validateField = validateField.bind(this);
+    this.validateForm = validateForm.bind(this);
+    this.populateErrors = populateErrors.bind(this);
+    this.formBusquedaSubmit = this.formBusquedaSubmit.bind(this);
   }
 
   columnsConfig = [
@@ -30,31 +63,90 @@ export default class Equipos extends React.Component {
   ];
 
   componentDidMount() {
-    this.fetchData();
+    this.fetchEquipos();
   }
 
-  fetchData() {
+  fetchEquipos() {
     this.props
       .makeRequest(
         {
           method: "get",
-          url: "/puntuacion-equipo/1/1"
+          url: "/teams"
         },
         true,
         false
       )
       .then(response => {
-        this.setState({ data: response.data.puntajes });
-      })
-      .catch(errors => {
-        console.log(errors);
+        this.setState({ equipos: response.data });
       });
   }
 
+  formBusquedaSubmit() {
+    if (this.validateForm("formBusqueda")) {
+      this.props
+        .makeRequest(
+          {
+            method: "get",
+            url: "/puntuacion-equipo/1/1"
+          },
+          true,
+          false
+        )
+        .then(response => {
+          this.setState({ data: response.data.puntajes });
+        })
+        .catch(errors => {
+          console.log(errors);
+        });
+    }
+  }
+
+  options = [
+    { id: 1, nombre: "Un juego" },
+    { id: 2, nombre: "Otro juego" },
+    { id: 3, nombre: "El tercer juego" },
+    { id: 4, nombre: "EL juego" }
+  ];
+
   render() {
-    const user = this.props.user;
     return (
       <DefaultCard>
+        <Row>
+          <Col xs="4">
+            <Select
+              label={"Juego"}
+              options={this.options}
+              valueAccessor={"id"}
+              labelAccessor={"nombre"}
+              onChange={this.updateForm("formBusqueda", "juego")}
+              onBlur={this.validateField("formBusqueda", "juego")}
+              errors={this.state.formBusqueda.juego.errors}
+              value={this.state.formBusqueda.juego.value}
+            />
+          </Col>
+          <Col xs="4">
+            <Select
+              label={"Equipo"}
+              options={this.state.equipos}
+              valueAccessor={"_id"}
+              labelAccessor={"nombre"}
+              onChange={this.updateForm("formBusqueda", "equipo")}
+              onBlur={this.validateField("formBusqueda", "equipo")}
+              errors={this.state.formBusqueda.equipo.errors}
+              value={this.state.formBusqueda.equipo.value}
+            />
+          </Col>
+          <Col xs={{ size: 2, offset: 2 }}>
+            <Button
+              block
+              color="success"
+              className="btn-square btn-sm"
+              onClick={this.formBusquedaSubmit}
+            >
+              Buscar
+            </Button>
+          </Col>
+        </Row>
         <Table data={this.state.data} columns={this.columnsConfig} />
       </DefaultCard>
     );
--- frontend/src/views/Puntuaciones/Juegos/Juegos.jsx
+++ frontend/src/views/Puntuaciones/Juegos/Juegos.jsx
@@ -1,13 +1,40 @@
 import React from "react";
 import DefaultCard from "../../../components/DefaultCard";
 import Table from "../../../components/STable";
+import Select from "../../../components/Select";
+import { Row, Col, Button } from "reactstrap";
+import {
+  updateForm,
+  validateField,
+  validateForm,
+  populateErrors
+} from "../../../lib/forms/utils";
+import { required } from "../../../lib/forms/validators";
 
 export default class Juegos extends React.Component {
+  updateForm;
+  updateField;
+  validateForm;
+  populateErrors;
+
   constructor(props) {
     super(props);
     this.state = {
-      data: []
+      data: [],
+      formBusqueda: {
+        juego: {
+          value: undefined,
+          errors: undefined,
+          validate: required()
+        }
+      }
     };
+
+    this.updateForm = updateForm.bind(this);
+    this.validateField = validateField.bind(this);
+    this.validateForm = validateForm.bind(this);
+    this.populateErrors = populateErrors.bind(this);
+    this.formBusquedaSubmit = this.formBusquedaSubmit.bind(this);
   }
 
   columnsConfig = [
@@ -29,32 +56,60 @@ export default class Juegos extends React.Component {
     }
   ];
 
-  componentDidMount() {
-    this.fetchData();
+  formBusquedaSubmit() {
+    if (this.validateForm("formBusqueda")) {
+      this.props
+        .makeRequest(
+          {
+            method: "get",
+            url: "/puntuacion-juego/1"
+          },
+          true,
+          false
+        )
+        .then(response => {
+          this.setState({ data: response.data.puntajes });
+        })
+        .catch(errors => {
+          console.log(errors);
+        });
+    }
   }
 
-  fetchData() {
-    this.props
-      .makeRequest(
-        {
-          method: "get",
-          url: "/puntuacion-juego/1"
-        },
-        true,
-        false
-      )
-      .then(response => {
-        this.setState({ data: response.data.puntajes });
-      })
-      .catch(errors => {
-        console.log(errors);
-      });
-  }
+  options = [
+    { id: 1, nombre: "Un juego" },
+    { id: 2, nombre: "Otro juego" },
+    { id: 3, nombre: "El tercer juego" },
+    { id: 4, nombre: "EL juego" }
+  ];
 
   render() {
-    const user = this.props.user;
     return (
       <DefaultCard>
+        <Row>
+          <Col xs="4">
+            <Select
+              label={"Juego"}
+              options={this.options}
+              valueAccessor={"id"}
+              labelAccessor={"nombre"}
+              onChange={this.updateForm("formBusqueda", "juego")}
+              onBlur={this.validateField("formBusqueda", "juego")}
+              errors={this.state.formBusqueda.juego.errors}
+              value={this.state.formBusqueda.juego.value}
+            />
+          </Col>
+          <Col xs={{ size: 2, offset: 6 }}>
+            <Button
+              block
+              color="success"
+              className="btn-square btn-sm"
+              onClick={this.formBusquedaSubmit}
+            >
+              Buscar
+            </Button>
+          </Col>
+        </Row>
         <Table data={this.state.data} columns={this.columnsConfig} />
       </DefaultCard>
     );
